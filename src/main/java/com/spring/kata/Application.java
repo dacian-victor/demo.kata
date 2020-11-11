555c106bb3e962971674e491496cf174d9ac82f2
dacian-victor <45403876+dacian-victor@users.noreply.github.com>
2020-11-11 20:51:48 +0200

    First Commit

---
+++ src/main/java/com/spring/kata/Application.java
@@ -0,0 +1,34 @@
+package com.spring.kata;
+
+
+import com.spring.kata.repository.Person;
+import com.spring.kata.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(PersonRepository repository) {
+        return strings -> {
+            repository.save(new Person("Daci", "Timisoara"));
+            repository.save(new Person("Aranud", "Timisoara"));
+            repository.save(new Person("Rony", "Timisoara"));
+        };
+    }
+
+}
---
+++ src/main/resources/static/precache-manifest.07eebb82a3ed79b4d4cf3aa59d29ca0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db3927694560b8892ddbc48cafc30448",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d96baab9a1accbce448b",
+    "url": "./static/css/main.8ce611bc.chunk.css"
+  },
+  {
+    "revision": "bc0ed6c5c9e649d64d47",
+    "url": "./static/js/2.3883c90d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3883c90d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d96baab9a1accbce448b",
+    "url": "./static/js/main.0d7ec78d.chunk.js"
+  },
+  {
+    "revision": "b12fb06c1b19e2084157",
+    "url": "./static/js/runtime-main.e89f0be9.js"
+  }
+]);
